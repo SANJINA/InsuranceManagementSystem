c7e5b6427fb0277b50d6d99fc45caa5d1ff0c795
SANJINA <sashakya548@gmail.com>
2023-08-23 13:23:06 -0500

    display policy functionality

--- src/com/insurance/main/PolicyManagementModule.java
+++ src/com/insurance/main/PolicyManagementModule.java
@@ -15,6 +15,7 @@ public class PolicyManagementModule {
      //Policy p1 = new Policy(101,"1011","Health Insurance",50000.0,50.0,"2022-10-10","2023-10-10","Active",true);
     //policyMap.put(101, p1); Deepak??
     //try new code
+    //added new method
 
 
     public static void displayPolicyMenu(){
