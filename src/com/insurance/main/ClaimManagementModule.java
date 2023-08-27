51f32daef0bb856da9867bbc9758e291f2a49a25
SANJINA <sashakya548@gmail.com>
2023-08-26 19:53:08 -0500

    fixed customer verification upon request

--- src/com/insurance/main/ClaimManagementModule.java
+++ src/com/insurance/main/ClaimManagementModule.java
@@ -3,4 +3,11 @@ package com.insurance.main;
 public class ClaimManagementModule {
 
     //store input data from Main class in map,
+
+    //Policy p1 = new Policy(101,"1011","Health Insurance",50000.0,50.0,"2022-10-10","2023-10-10","Active",true);
+    //policyMap.put(101, p1); Deepak??
+    //try new code
+    //added new method
+    //added new search functionality
+    //added new update address functionality
 }
--- src/com/insurance/model/Claim.java
+++ src/com/insurance/model/Claim.java
@@ -11,6 +11,13 @@ public class Claim {
     private String dateResolved;
     private String policyNumber;
 
+    //Policy p1 = new Policy(101,"1011","Health Insurance",50000.0,50.0,"2022-10-10","2023-10-10","Active",true);
+    //policyMap.put(101, p1); Deepak??
+    //try new code
+    //added new method
+    //added new search functionality
+    //added new update address functionality
+
     public Claim(String claimNumber, String dateField, String description, String state, double amount, String dateResolved, String policyNumber) {
         this.claimNumber = claimNumber;
         this.dateField = dateField;
