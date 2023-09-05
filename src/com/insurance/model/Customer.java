10d7dbce5ae75dff2e523be5e0e4108957cad3a9
SANJINA <sashakya548@gmail.com>
2023-09-05 11:31:45 -0500

    verify address

--- src/com/insurance/model/Customer.java
+++ src/com/insurance/model/Customer.java
@@ -23,6 +23,18 @@ public class Customer {
     //added new method
     //added new search functionality
     //added new update address functionality
+    //Policy p1 = new Policy(101,"1011","Health Insurance",50000.0,50.0,"2022-10-10","2023-10-10","Active",true);
+    //policyMap.put(101, p1); Deepak??
+    //try new code
+    //added new method
+    //added new search functionality
+    //added new update address functionality
+    //Policy p1 = new Policy(101,"1011","Health Insurance",50000.0,50.0,"2022-10-10","2023-10-10","Active",true);
+    //policyMap.put(101, p1); Deepak??
+    //try new code
+    //added new method
+    //added new search functionality
+    //added new update address functionality
 
 
     //Policy p1 = new Policy(101,"1011","Health Insurance",50000.0,50.0,"2022-10-10","2023-10-10","Active",true);
