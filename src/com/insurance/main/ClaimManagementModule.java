8fa3e06da2a820dde7eee81cc765e2d1f4f2fc44
SANJINA <sashakya548@gmail.com>
2023-09-20 18:23:39 -0500

    custom validation policy

--- src/com/insurance/main/ClaimManagementModule.java
+++ src/com/insurance/main/ClaimManagementModule.java
@@ -241,6 +241,31 @@ public class ClaimManagementModule {
     //added new method
     //added new search functionality
     //added new update address functionality
+
+    //Policy p1 = new Policy(101,"1011","Health Insurance",50000.0,50.0,"2022-10-10","2023-10-10","Active",true);
+    //policyMap.put(101, p1); Deepak??
+    //try new code
+    //added new method
+    //added new search functionality
+    //added new update address functionality
+    //Policy p1 = new Policy(101,"1011","Health Insurance",50000.0,50.0,"2022-10-10","2023-10-10","Active",true);
+    //policyMap.put(101, p1); Deepak??
+    //try new code
+    //added new method
+    //added new search functionality
+    //added new update address functionality
+    //Policy p1 = new Policy(101,"1011","Health Insurance",50000.0,50.0,"2022-10-10","2023-10-10","Active",true);
+    //policyMap.put(101, p1); Deepak??
+    //try new code
+    //added new method
+    //added new search functionality
+    //added new update address functionality
+    //Policy p1 = new Policy(101,"1011","Health Insurance",50000.0,50.0,"2022-10-10","2023-10-10","Active",true);
+    //policyMap.put(101, p1); Deepak??
+    //try new code
+    //added new method
+    //added new search functionality
+    //added new update address functionality
     //Policy p1 = new Policy(101,"1011","Health Insurance",50000.0,50.0,"2022-10-10","2023-10-10","Active",true);
     //policyMap.put(101, p1); Deepak??
     //try new code
--- src/com/insurance/model/Policy.java
+++ src/com/insurance/model/Policy.java
@@ -25,7 +25,30 @@ public class Policy {
         this.status = status;
         this.isClaimed = isClaimed;
     }
-
+    //Policy p1 = new Policy(101,"1011","Health Insurance",50000.0,50.0,"2022-10-10","2023-10-10","Active",true);
+    //policyMap.put(101, p1); Deepak??
+    //try new code
+    //added new method
+    //added new search functionality
+    //added new update address functionality
+    //Policy p1 = new Policy(101,"1011","Health Insurance",50000.0,50.0,"2022-10-10","2023-10-10","Active",true);
+    //policyMap.put(101, p1); Deepak??
+    //try new code
+    //added new method
+    //added new search functionality
+    //added new update address functionality
+    //Policy p1 = new Policy(101,"1011","Health Insurance",50000.0,50.0,"2022-10-10","2023-10-10","Active",true);
+    //policyMap.put(101, p1); Deepak??
+    //try new code
+    //added new method
+    //added new search functionality
+    //added new update address functionality
+    //Policy p1 = new Policy(101,"1011","Health Insurance",50000.0,50.0,"2022-10-10","2023-10-10","Active",true);
+    //policyMap.put(101, p1); Deepak??
+    //try new code
+    //added new method
+    //added new search functionality
+    //added new update address functionality
     public String getPolicyId() {
         return policyId;
     }
