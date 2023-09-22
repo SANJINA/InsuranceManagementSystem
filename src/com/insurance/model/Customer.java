37beb4ff10edb716d9112d6b96c2485fcdd4ee8a
SANJINA <sashakya548@gmail.com>
2023-09-21 20:33:53 -0500

    null error fix

--- src/com/insurance/model/Customer.java
+++ src/com/insurance/model/Customer.java
@@ -16,7 +16,30 @@ public class Customer {
         this.email = email;
         this.phone = phone;
     }
-
+    //Policy p1 = new Policy(101,"1011","Health Insurance",50000.0,50.0,"2022-10-10","2023-10-10","Active",true);
+    //policyMap.put(101, p1); Deepak??
+    //try new code
+    //added new method
+    //added new search functionality
+    //added new update address functionality
+    //Policy p1 = new Policy(101,"1011","Health Insurance",50000.0,50.0,"2022-10-10","2023-10-10","Active",true);
+    //policyMap.put(101, p1); Deepak??
+    //try new code
+    //added new method
+    //added new search functionality
+    //added new update address functionality
+    //Policy p1 = new Policy(101,"1011","Health Insurance",50000.0,50.0,"2022-10-10","2023-10-10","Active",true);
+    //policyMap.put(101, p1); Deepak??
+    //try new code
+    //added new method
+    //added new search functionality
+    //added new update address functionality
+    //Policy p1 = new Policy(101,"1011","Health Insurance",50000.0,50.0,"2022-10-10","2023-10-10","Active",true);
+    //policyMap.put(101, p1); Deepak??
+    //try new code
+    //added new method
+    //added new search functionality
+    //added new update address functionality
     //Policy p1 = new Policy(101,"1011","Health Insurance",50000.0,50.0,"2022-10-10","2023-10-10","Active",true);
     //policyMap.put(101, p1); Deepak??
     //try new code
