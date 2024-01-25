fd74d7c013264213ff079bed042c4fd06ab1d045
SANJINA <sashakya548@gmail.com>
2024-01-25 13:17:20 -0600

    fixed cutomer fetch query

--- src/com/insurance/main/CustomerManagementModule.java
+++ src/com/insurance/main/CustomerManagementModule.java
@@ -39,6 +39,13 @@ public class CustomerManagementModule {
     //added new method
     //added new search functionality
     //added new update address functionality
+
+    //Policy p1 = new Policy(101,"1011","Health Insurance",50000.0,50.0,"2022-10-10","2023-10-10","Active",true);
+    //policyMap.put(101, p1); Deepak??
+    //try new code
+    //added new method
+    //added new search functionality
+    //added new update address functionality
     //Policy p1 = new Policy(101,"1011","Health Insurance",50000.0,50.0,"2022-10-10","2023-10-10","Active",true);
     //policyMap.put(101, p1); Deepak??
     //try new code
